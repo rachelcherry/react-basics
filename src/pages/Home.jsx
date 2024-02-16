fa6a037d804aa053fde23b5389792ab3443ab84d
rachelcherry <rcherry@bu.edu>
2024-02-16 11:14:25 -0500

    added: text from the example and a myButton function that will handle the button for signing up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 export function Home() {
+  function MyButton({ onClick }) {
+    return (
+      <button>Add</button>
+    );
+  }
   return (
     <div>
       <div>
         <h1> Welcome to Spark! BookPals</h1>
-        <p>You will update stuff in here.</p>
+        <p>Spark! BookPals is a revolutionary platform for Boston University's community. BU students and affiliates can connect with each other to share book recommendations, start in-depth discussions, and more! Our unique features allow users to filter books based on genres and complexity, among other things. Stay up-to-date through SMS, Signal, and whatsapp notifications wherever a book you may be interested in is disussed</p>
       </div>
-    </div>
-  );
-}
+      <div>
+        <h2>Sign up</h2>
+        <p>Sign up for notifications</p>
+</div>
+  </div>
+    )
+    }
