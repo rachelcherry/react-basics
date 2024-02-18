c56d3315f6b4ee661ba41093d17d2774522234fc
rachelcherry <rcherry@bu.edu>
2024-02-18 00:43:49 -0500

    added the lines from the example and a photo of myself (not in a circle yet)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,23 @@
 import "../index.css";
 
 export function About() {
-  return <>I&apos;m a sad empty page. Build me</>;
+
+  return (
+    <div>
+    <h1>About Spark! Bookpals!</h1>
+    <p>Spark! BookPals was conceived with a simple idea in mind - to make it easy for Boston University community members to find new books to read. We recognized the potential of connecting those who have read a lot with those are just dipping their feet into reading.</p>
+    <div>
+      <h1>{"About the Author"}</h1>
+      <img
+        className="avatar"
+        src={'me.png'} 
+        style={{
+          width: 90,
+          height: 90
+        }}
+      />
+      <p></p>
+      </div>
+    </div>
+  )
 }
